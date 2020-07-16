2a11155781d56922ba11a0fed2ed39c9db867dc4
wilberever100 <wilber.correa@ucsp.edu.pe>
2020-07-15 21:39:10 -0500

    Mutex while busy error

--- source/main.cpp
+++ source/main.cpp
@@ -4,175 +4,209 @@
 #include <random>
 #include <thread>
 #include <vector>
+#include <ctime>
+#include <sstream>
+
+
 
 #include "concurrent_skip_list.hpp"
 #include "skip_list.hpp"
 
+
+using namespace std;
+
 int get_random(int lower, int higher) {
-  std::random_device rd;
-  std::mt19937 gen(rd());
-  std::uniform_int_distribution<> distribution(lower, higher);
-  return distribution(gen);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distribution(lower, higher);
+    return distribution(gen);
 }
 
 struct IntWithObjCounter {
-  explicit IntWithObjCounter(int n = 0) : num(n) { ++counter; }
-  IntWithObjCounter(const IntWithObjCounter& o) : num(o.num) { ++counter; }
-  ~IntWithObjCounter() { --counter; }
-  int num;
-  static unsigned int counter;
+    explicit IntWithObjCounter(int n = 0) : num(n) { ++counter; }
+    IntWithObjCounter(const IntWithObjCounter& o) : num(o.num) { ++counter; }
+    ~IntWithObjCounter() { --counter; }
+    int num;
+    static unsigned int counter;
 };
+bool operator<(const IntWithObjCounter& left, const IntWithObjCounter& right) {
+    return left.num < right.num;
+}
+bool operator==(const IntWithObjCounter& left, const IntWithObjCounter& right) {
+    return left.num == right.num;
+}
+bool operator!=(const IntWithObjCounter& left, const IntWithObjCounter& right) {
+    return left.num != right.num;
+}
 
 unsigned int IntWithObjCounter::counter = 0;
-
+/*
 bool operator<(const IntWithObjCounter& left, const IntWithObjCounter& right) {
   return left.num < right.num;
 }
 bool operator==(const IntWithObjCounter& left, const IntWithObjCounter& right) {
   return left.num == right.num;
-}
+}*/
 
 void TestSequential() {
-  std::cout << "Basic test on consurrent SkipListSet" << std::endl
-            << "====================================" << std::endl;
-  {
-    // no dangling pointers test
-    int num_iter = 1000;
-    ADE::Concurrent::ConcurrentSkipList<IntWithObjCounter> test;
-    for (int i = 1; i < num_iter; ++i) test.insert(num_iter);
-    for (int i = 1; i < num_iter; ++i) test.remove(num_iter);
-    assert(IntWithObjCounter::counter == 2);  // sentinelas left y right
-  }
+    std::cout << "Basic test on consurrent SkipListSet" << std::endl
+        << "====================================" << std::endl;
+    {
+        // no dangling pointers test
+        int num_iter = 1000;
+        ADE::Concurrent::ConcurrentSkipList<IntWithObjCounter> test;
+        for (int i = 1; i < num_iter; ++i) test.insert(IntWithObjCounter(num_iter));
+        IntWithObjCounter::counter;
+        for (int i = 1; i < num_iter; ++i) test.remove(IntWithObjCounter(num_iter));
 
-  {
-    // inserción y eliminación aleatoria
-    ADE::Concurrent::ConcurrentSkipList<int> test(10);
+        assert(IntWithObjCounter::counter == 1);  // sentinelas left y right
+    }
 
-    int max_value = 1000;
-    int num_iter = 1000;
+    {
+        // inserción y eliminación aleatoria
+        ADE::Concurrent::ConcurrentSkipList<int> test(10);
 
-    for (int i = 1; i < num_iter; ++i) test.insert(get_random(0, max_value));
-    for (int i = 1; i < num_iter; ++i) test.remove(get_random(0, max_value));
+        int max_value = 1000;
+        int num_iter = 1000;
 
-    test.debug_print(std::cout);
-    std::cout << std::endl;
-  }
+        for (int i = 1; i < num_iter; ++i) test.insert(get_random(0, max_value));
+        for (int i = 1; i < num_iter; ++i) test.remove(get_random(0, max_value));
 
-  {
-    // validación de la no-doble eliminación
-    ADE::Concurrent::ConcurrentSkipList<int> test(10);
-    test.insert(1);
-    test.insert(2);
-    test.insert(3);
-    test.remove(3);
-    test.remove(2);
-    test.remove(1);
-    assert(!test.contains(1));
-  }
+        test.debug_print();
+        std::cout << std::endl;
+    }
 
-  {
-    // validación de la no-doble inserción
-    ADE::Concurrent::ConcurrentSkipList<int> test(10);
-    test.insert(1);
-    test.insert(2);
-    test.insert(3);
-    test.insert(4);
-    test.insert(5);
-    assert(!test.insert(3));
+    {
+        // validación de la no-doble eliminación
+        ADE::Concurrent::ConcurrentSkipList<int> test(10);
+        test.insert(1);
+        test.insert(2);
+        test.insert(3);
+        test.remove(3);
+        test.remove(2);
+        test.remove(1);
+        assert(!test.contains(1));
+    }
 
-    // validación de la inserción y eliminación
-    test.remove(3);
-    assert(test.insert(3));
-    test.remove(3);
-    assert(!test.contains(3));
-    assert(test.contains(5));
-  }
+    {
+        // validación de la no-doble inserción
+        ADE::Concurrent::ConcurrentSkipList<int> test(10);
+        test.insert(1);
+        test.insert(2);
+        test.insert(3);
+        test.insert(4);
+        test.insert(5);
+        assert(!test.insert(3));
+
+        // validación de la inserción y eliminación
+        test.remove(3);
+        assert(test.insert(3));
+        test.remove(3);
+        assert(!test.contains(3));
+        assert(test.contains(5));
+    }
 }
 
 class ListTester {
- public:
-  static const int MAX_NUM_OPERATIONS = 10000;
-  static int MIN_VALS;
-  static int MAX_VALS;
+public:
+    static const int MAX_NUM_OPERATIONS = 10000;
+    static int MIN_VALS;
+    static int MAX_VALS;
 
- public:
-  ListTester(SkipListSet<int>& lista, int modifier)
-      : lista_(lista), modifier_(modifier) {}
-  void operator()() {
-    static const double CONTAINS_WEIGHT = 0.2;
-    static const double INSERT_WEIGHT = 0.7;
+public:
+    ListTester(ADE::Concurrent::ConcurrentSkipList<int>& lista, int modifier)
+        : lista_(lista), modifier_(modifier) {}
+    void operator()() {
+        static const double CONTAINS_WEIGHT = 0.2;
+        static const double INSERT_WEIGHT = 0.7;
 
-    for (int i = 0; i < MAX_NUM_OPERATIONS; ++i) {
-      if (sample_01_() < CONTAINS_WEIGHT) {
-        lista_.contains(random_());
-      } else {
-        if (sample_01_() < INSERT_WEIGHT)
-          lista_.insert(random_());
-        else
-          lista_.remove(random_());
-      }
+        for (int i = 0; i < MAX_NUM_OPERATIONS; ++i) {
+            std::random_device rd; // random device engine, usually based on /dev/random on UNIX-like systems
+            // initialize Mersennes' twister using rd to generate the seed
+            std::mt19937 rng(rd());
+
+            if (sample_01_(rng) < CONTAINS_WEIGHT) {
+                lista_.contains(random_(rng));
+            }
+            else {
+                if (sample_01_(rng) < INSERT_WEIGHT)
+                    lista_.insert(random_(rng));
+                else
+                    lista_.remove(random_(rng));
+            }
+        }
     }
-  }
 
- private:
-  ADE::Concurrent::ConcurrentSkipListSet<int>& lista_;
-  int modifier_;
+private:
+    ADE::Concurrent::ConcurrentSkipList<int>& lista_;
+    int modifier_;
 
-  static std::variate_generator<std::mt19937, std::uniform_real<double> >
-      sample_01_;
-  static std::variate_generator<std::mt19937, std::uniform_int<unsigned int> >
-      random_;
+    static std::uniform_real_distribution <double>
+        sample_01_;
+    static std::uniform_int_distribution<unsigned int>
+        random_;
 };
 
 int ListTester::MIN_VALS = 1;
 int ListTester::MAX_VALS = 15000;
 
-std::variate_generator<std::mt19937, std::uniform_real<double> >
+/*std::uniform_real_distribution<double>
     ListTester::sample_01_(
-        std::mt19937(static_cast<unsigned long>(std::time(NULL))),
-        std::uniform_real<double>());
-std::variate_generator<std::mt19937, std::uniform_int<unsigned int> >
+        static_cast<double>(std::mt19937(static_cast<unsigned long>(std::time(NULL)))),
+        static_cast<double> std::uniform_real<double>());
+
+std::uniform_real_distribution<std::mt19937, std::uniform_int<unsigned int> >
     ListTester::random_(
         std::mt19937(static_cast<unsigned long>(std::time(NULL))),
         std::uniform_int<unsigned int>(ListTester::MIN_VALS,
                                        ListTester::MAX_VALS));
+*/
+std::uniform_real_distribution<double>
+ListTester::sample_01_(
+    0.0,
+    1.0);
+
+std::uniform_int_distribution<unsigned int>
+ListTester::random_(
+    MIN_VALS,
+    MAX_VALS);
 
 void RunTest(bool print) {
-  int num_threads = 8;
+    int num_threads = 8;
 
-  ADE::Concurrent::ConcurrentSkipList<int> lista;
+    ADE::Concurrent::ConcurrentSkipList<int> lista;
 
-  std::vector<std::thread*> threads;
+    std::vector<std::thread*> threads;
 
-  for (int i = 0; i < num_threads; i++) {
-    ListTester tester(lista, i * ListTester::MAX_NUM_OPERATIONS);
-    std::thread* process = new std::thread(tester);
-    threads.push_back(process)
-  }
-  for (int i = 0; i < num_threads; ++i) {
-    threads[i]->join();
-  }
+    for (int i = 0; i < num_threads; i++) {
+        ListTester tester(lista, i * ListTester::MAX_NUM_OPERATIONS);
+        std::thread* process = new std::thread(tester);
+        threads.push_back(process);
+    }
+    for (int i = 0; i < num_threads; ++i) {
+        threads[i]->join();
+    }
 
-  if (print) {
-    cout << "Test on consurrent SkipListSet" << endl
-         << "==============================" << endl;
+    if (print) {
+        cout << "Test on consurrent SkipListSet" << endl
+            << "==============================" << endl;
 
-    stringstream os;
-    os << endl;
-    lista.debug_print(os);
-    os << endl;
-    cout << os.str();
-  }
+        std::stringstream os;
+        os << endl;
+        lista.debug_print();
+        os << endl;
+        cout << os.str();
+    }
 }
 
 int main() {
-  freopen("output.txt", "w", stdout);
-  TestSequential();
-  const int trials = 10;
-  for (int i = 0; i < trials; ++i) {
-    cerr << "Running test case " << i << endl;
-    RunTest(i == trials - 1);
-  }
+    freopen("output.txt", "w", stdout);
+    TestSequential();
+    const int trials = 10;
+    for (int i = 0; i < trials; ++i) {
+        std::cout << "Running test case " << i << endl;
+        RunTest(i == trials - 1);
+    }
 
-  return 0;
+    return 0;
 }
--- source/skip_list/concurrent_skip_list.hpp
+++ source/skip_list/concurrent_skip_list.hpp
@@ -6,51 +6,224 @@
 #include <iostream>
 #include <random>
 #include <vector>
+#include <mutex>
+
 
 namespace ADE {
-namespace Concurrent {
+    namespace Concurrent {
 
-template <typename Type, class Comparator = std::less<Type>>
-class SkipList {
- public:
-  typedef Type data_type;
-  Comparator compare_;
 
-  bool get_random_bool() {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> distribution(0, 1);
-    return distribution(gen);
-  }
 
-  struct Node {
-    Node(unsigned int level, const data_type& data)
-        : forward_(level, nullptr), value_(data) {}
-    std::vector<Node*> forward_;
-    data_type value_;
-  };
+        template <typename Type, class Comparator = std::less<Type>>
+        class ConcurrentSkipList {
+        public:
+            typedef Type data_type;
+            Comparator compare_;
 
-  explicit SkipList(unsigned int max_level = 16)
-      : max_level_(max_level),
-        current_level_(1),
-        header_(max_level_, data_type()) {}
+            bool get_random_bool() {
+                std::random_device rd;
+                std::mt19937 gen(rd());
+                std::uniform_int_distribution<> distribution(0, 1);
+                return distribution(gen);
+            }
 
-  void debug_print() {
-    Node* it = &header_;
-    while (it->forward_[0] != nullptr) {
-      for (unsigned int i = 0; i < it->forward_.size(); ++i) {
-        std::cout << it->forward_[0]->value_ << " ";
-      }
-      std::cout << std::endl;
-      it = it->forward_[0];
-    }
-  }
+            struct Node {
+                Node(unsigned int level, const data_type& data)
+                    : forward_(level, nullptr), value_(data) {}
+                std::vector<Node*> forward_;
+                data_type value_;
+                std::mutex source_;
+            };
 
-  unsigned int max_level_;
-  unsigned int current_level_;
-  Node header_;
-};
-}  // namespace Concurrent
+            explicit ConcurrentSkipList(unsigned int max_level = 16)
+                : max_level_(max_level),
+                current_level_(1),
+                header_(max_level_, data_type()) {}
+
+            void debug_print() {
+                Node* it = &header_;
+                while (it->forward_[0] != nullptr) {
+                    for (unsigned int i = 0; i < it->forward_.size(); ++i) {
+                        std::cout << it->forward_[0]->value_ << " ";
+                    }
+                    std::cout << std::endl;
+                    it = it->forward_[0];
+                }
+            }
+            unsigned int size() const {
+                unsigned int size = 0;
+                Node* it = header_.forward_[0];
+                while (it != nullptr) {
+                    ++size;
+                    it = it->forward_[0];
+                }
+                return size;
+            }
+
+            void unlocking_nodes(std::vector<Node*>& predecessor) {
+                Node* back = nullptr;
+                for (unsigned int i = 0; i < predecessor.size(); i++) {
+                    if (predecessor[i]) {
+                        if (predecessor[i] != back) {
+                            predecessor[i]->source_.unlock();
+                        }
+
+                        back = predecessor[i];
+                    }
+                    else {
+                        break;
+                    }
+                }
+            }
+
+            bool insert(const data_type& data) {
+                //source.lock();
+                std::vector<Node*> predecessor;
+                bool node_locked;
+                Node* it = find(data, &predecessor, node_locked);
+
+                if (it != nullptr && it->value_ == data) {
+                    //source.unlock();
+
+                    unlocking_nodes(predecessor);
+                    it->source_.unlock();
+
+                    return false;
+                }
+
+                unsigned int level = 1;
+                while (get_random_bool() && level < max_level_) {
+                    ++level;
+                }
+
+                if (current_level_ < level) {
+                    bool should_lock = true;
+                    for (unsigned int i = 0; i < predecessor.size(); i++) {
+                        if (predecessor[i] == &header_) {
+                            should_lock = false;
+                            break;
+                        }
+                    }
+
+                    predecessor[current_level_] = &header_;
+
+                    level = ++current_level_;
+
+                    if (should_lock) {
+                        header_.source_.lock();
+                    }
+                }
+
+                Node* new_node = new Node(level, data_type(data));
+                for (unsigned int i = 0; i < level; ++i) {
+                    new_node->forward_[i] = predecessor[i]->forward_[i];
+                    predecessor[i]->forward_[i] = new_node;
+                }
+
+
+
+
+                unlocking_nodes(predecessor);
+
+
+                //source.unlock();
+                return true;
+            }
+            bool remove(const data_type& data) {
+                std::vector<Node*> predecessor;
+                bool node_locked;
+                Node* it = find(data, &predecessor, node_locked);
+
+                if (it == nullptr || it->value_ != data) {
+                    unlocking_nodes(predecessor);
+                    return false;
+                }
+
+                for (unsigned int i = 0; i < current_level_; ++i) {
+                    if (predecessor[i]->forward_[i] != it) {
+                        break;
+                    }
+                    predecessor[i]->forward_[i] = it->forward_[i];
+                }
+                unlocking_nodes(predecessor);
+
+                if (node_locked) {
+                    it->source_.unlock();
+                }
+
+                delete it;
+                while (1 < current_level_ && header_.forward_[current_level_ - 1] == 0) {
+                    --current_level_;
+                }
+                return true;
+            }
+            bool contains(const data_type& data) {
+                std::vector<Node*> predecessor;
+                bool x;
+                Node* it = find(data, &predecessor, x);
+
+                return (it != nullptr && it->value_ == data);
+            }
+            void clear() {
+                Node* it = header_.forward_[0];
+                while (it != nullptr) {
+                    Node* ptr = it->forward_[0];
+                    delete it;
+                    it = ptr;
+                }
+                for (unsigned int i = 0; i < current_level_; ++i) {
+                    header_.forward_[i] = nullptr;
+                }
+                current_level_ = 1;
+            }
+            bool empty() const { return header_.forward_[0] == nullptr; }
+
+
+            unsigned int max_level_;
+            unsigned int current_level_;
+            Node header_;
+
+        private:
+            Node* find(const data_type& data, std::vector<Node*>* predecessor, bool& found_first) {
+                predecessor->resize(max_level_, nullptr);
+
+                Node* it = &header_, * back = nullptr;
+                found_first = false;
+                for (int i = current_level_ - 1; 0 <= i; --i) {
+                    //This node will be locking while traveling
+                    // Node* looking_for = nullptr;
+                    while (it->forward_[i] != nullptr &&
+                        compare_(it->forward_[i]->value_, data)) {
+
+                        it = it->forward_[i];
+                    }
+
+                    if (it->forward_[i] && it->forward_[i]->value_ == data && !found_first) {
+                        found_first = true;
+
+                        if (!(it->forward_[i]->source_.try_lock())) {
+                            it = back;
+                            i++;
+                            found_first = false;
+                            continue;
+                        }
+                    }
+                    if (back != it && it) {
+
+                        it->source_.lock();
+
+
+                    }
+
+                    (*predecessor)[i] = it;
+                    back = it;
+                }
+                it = it->forward_[0];
+
+                return it;
+            }
+        };
+    }  // namespace Concurrent
 }  // namespace ADE
 
 #endif  // SOURCE_SKIP_LIST_CONCURRENT_SKIP_LIST_HPP_
